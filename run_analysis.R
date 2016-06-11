853a1025dd1f64fa0c33736ceddc3debe01d530f
Hatsumi <hh0749@hotmail.com>
2016-06-11 11:32:50 -0400

    update

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+##Getting and Cleaning Data Project code
+
+##get the data into R
+path_rf <-"/Users/hatsumi/Documents/coursera/Cleaning\ data/UCI\ HAR\ Dataset" 
+files<-list.files(path_rf, recursive=TRUE)
+
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+
+##Merge the dataset and change variable names
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+##Get only mean() and STR()
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+##Rename activity labels
+Data$activity <- as.character(Data$activity)
+Data$activity[Data$activity == 1] <- "Walking"
+Data$activity[Data$activity == 2] <- "Walking Upstairs"
+Data$activity[Data$activity == 3] <- "Walking Downstairs"
+Data$activity[Data$activity == 4] <- "Sitting"
+Data$activity[Data$activity == 5] <- "Standing"
+Data$activity[Data$activity == 6] <- "Laying"
+Data$activity <- as.factor(Data$activity)
+
+##Change labels
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+##output dataset
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
